d9c34121da17eaf64a6cca73abe8e33f4a9f44ec
RubenA2109 <a14685@oficina.pt>
2026-04-27 11:52:31 +0000

    projeto

--- Testecsharp/Program.cs
+++ Testecsharp/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddHttpClient();
+
+builder.Services.AddHttpClient("RestCountries", c =>
+{
+    c.BaseAddress = new Uri("https://restcountries.com/");
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    return new HttpClientHandler
+    {
+        SslProtocols = System.Security.Authentication.SslProtocols.Tls12
+    };
+});
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
--- Testecsharp/obj/Debug/net10.0/Testecsharp.AssemblyInfo.cs
+++ Testecsharp/obj/Debug/net10.0/Testecsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testecsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4109694499cc44ea6a5149089bdc1c645ce0ba43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cff0d742f7ba5aa1297a7ad3e827844d5a630676")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testecsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testecsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
